abe6a8752399831efd0dcead79a7af14f4211a7a
KJnong <nongjethro@gmail.com>
2020-10-02 04:41:32 +0200

    Improved HomePage feel

--- Controllers/VehicleController.cs
+++ Controllers/VehicleController.cs
@@ -66,7 +66,7 @@ namespace CapiMotors.Controllers
                 Vehicle = vehicle
             };
             
-            return View("VehicleInfo", vehicleInfoViewModel);
+            return View("VehicleDetails", vehicleInfoViewModel);
         }
 
 
